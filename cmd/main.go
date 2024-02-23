5db02e898f796fa52c708c2e90095ce59d9c468c
rovezuka <rostislav.04@list.ru>
2024-02-23 14:44:20 +0300

    clean architecture and add viperConfig

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,25 @@ import (
 	"log"
 	"todo"
 	"todo/pkg/handler"
+	"todo/pkg/repository"
+	"todo/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
